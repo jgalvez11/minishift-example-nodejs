bcc83c46681fad289ff278c6db2a3ea0c7a2f923
Galvez Casanova <jagalvezc@indracompany.com>
2020-03-20 08:31:24 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(5000, err => {
+  if (err) {
+    return console.log("ERROR Listenning server");
+  }
+
+  return console.log("SUCCESS Listenning server");
+});
+
+app.get("/", (req, res) => {
+  res.send({ status: "OK", message: "Servicio Rest de prueba" });
+  res.end();
+});
